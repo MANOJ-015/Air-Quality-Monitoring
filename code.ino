71a0599c3b81ec4467d26270c1cb9ce369348d64
Manoj <65275028+MANOJ-015@users.noreply.github.com>
2022-01-02 17:00:11 +0530

    Updated the code.ino
    Added co2 level feauture

--- code.ino
+++ code.ino
@@ -4,9 +4,10 @@ LiquidCrystal lcd(rs,en,d4,d5,d6,d7); //lcd function from LiquidCrystal
 
 int buz = 8;  //buzzer connected to pin 8
 int led = 9;  //led connected to pin 9
+int gas,co2lvl,ppm;
 
 const int aqsensor = A0;  //output of mq135 connected to A0 pin of Arduino
-int threshold = 125;      //Threshold level for Air Quality
+int threshold = 450;      //Threshold level for Air Quality
 
 void setup() {
 
@@ -22,30 +23,40 @@ void setup() {
 
 void loop() {
 
-  int ppm = analogRead(aqsensor); //read MQ135 analog outputs at A0 and store it in ppm
+  gas = analogRead(aqsensor); //read MQ135 analog outputs at A0 and store it in ppm
+  co2lvl = gas - 120;
+  ppm = map(co2lvl,0,1024,400,5000);
 
   Serial.print("Air Quality: ");  //print message in serail monitor
   Serial.println(ppm);            //print value of ppm in serial monitor
 
   lcd.setCursor(0,0);             // set cursor of lcd to 1st row and 1st column
-  lcd.print("Air Qualit: ");      // print message on lcd
+  lcd.print("Air Quality: ");      // print message on lcd
   lcd.print(ppm);                 // print value of MQ135
 
-  if (ppm > threshold)            // check is ppm is greater than threshold or not
+  if (ppm >= threshold && ppm<=1000)            // check is ppm is greater than threshold or not
     {
       lcd.setCursor(1,1);         //jump here if ppm is greater than threshold
-      lcd.print("AQ Level HIGH");
-      Serial.println("AQ Level HIGH");     
+      lcd.print("Max Acceptable Co2");
+      Serial.println("Max Acceptable Co2");     
       tone(led,1000,200);         //blink led with turn on time 1000mS, turn off time 200mS
       digitalWrite(buz,HIGH);     //Turn ON Buzzer
     }
+  else if(ppm<=threshold && ppm>=350)
+  {
+      digitalWrite(led,LOW);   //jump here if ppm is not greater than threshold and turn off LED
+      digitalWrite(buz,LOW);   //Turn off Buzzer
+      lcd.setCursor(1,1);
+      lcd.print ("Avg Indoor Co2");
+      Serial.println("Avg Indoor Co2");
+  }
   else
     {
       digitalWrite(led,LOW);   //jump here if ppm is not greater than threshold and turn off LED
       digitalWrite(buz,LOW);   //Turn off Buzzer
       lcd.setCursor(1,1);
-      lcd.print ("AQ Level Good");
-      Serial.println("AQ Level Good");
+      lcd.print ("Dangerous Co2 levels");
+      Serial.println("Dangerous Co2 levels");
     }  
   delay (500);
 }
